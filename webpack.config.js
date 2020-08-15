934d28a8ef3e6a5a488d7fc13c56455fd2d0699e
nickolasBenakis <nickolasbenele@gmail.com>
2020-08-16 00:58:40 +0300

    Add dotenvs, clean webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: process.env.DEV ? 'development' : 'production',
+  devtool: process.env.DEV ? 'inline-source-map' : false,
   entry: {
-    app: './src/index.js',
     util: './src/util.js',
+    app: './src/index.js',
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
+      inject: true,
+      template: require('html-webpack-template'),
       title: 'Output Management',
+      appMountId: 'root',
     }),
   ],
   module: {
