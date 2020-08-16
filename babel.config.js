8a570d2f63b37e4485eb48c528f1a4b1469485db
nickolasBenakis <nickolasbenele@gmail.com>
2020-08-16 22:18:01 +0300

    Add babel-loader

--- babel.config.js
+++ babel.config.js
@@ -1,1 +1,19 @@
-module.exports = {};
+module.exports = {
+  presets: [['@babel/preset-env'], '@babel/preset-react'],
+  plugins: [],
+  env: {
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+      ],
+      plugins: [],
+    },
+  },
+};
